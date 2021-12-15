765238866172105b3049ba2fceed13f678e37311
deviant-syndrome <86923754+deviant-syndrome@users.noreply.github.com>
2021-12-15 21:39:50 +0200

    Added simple bash script to copy files from this repo to default Inkscape extensions folder on MacOS

---
+++ deploy-macos.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp export_layers.py ~/Library/ApplicationSupport/org.inkscape.Inkscape/config/inkscape/extensions
+cp export_layers.inx ~/Library/ApplicationSupport/org.inkscape.Inkscape/config/inkscape/extensions
